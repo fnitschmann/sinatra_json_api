a72185087b641991addb852385c98fca0f5a424c
Florian Nitschmann <f.nitschmann@googlemail.com>
2014-12-08 18:22:25 +0100

    more updates for Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,30 +1,22 @@
-require "capistrano/bundler"
-require "capistrano/rbenv"
-
 set :rbenv_type, :user
-set :rbenv_ruby "2.1.3"
+set :rbenv_ruby, "2.1.3"
 
 set :rack_env, :production
 
 set :user, "deploy"
 set :domain, "customers.projects.nitschmann.io"
 set :application, "customers_api"
-set :deploy_to, "/home/deploy/#{application}"
+set :deploy_to, "/home/deploy/customers_api"
 set :scm, "git"
-set :repo_url "git@github.com:fnitschmann/json_api_sample.git"
+set :repo_url, "git@github.com:fnitschmann/json_api_sample.git"
 set :branch, "master"
 
-ssh_options[:forward_agent] = true
-ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "id_rsa")]
-ssh_options[:paranoid] = false
-default_run_options[:pty] = true
+set :keep_releases, 3
+
+set :linked_files, %w{config.yml}
+set :linked_dirs, %w{bin db}
 
 namespace :deploy do
-  desc 'restart the app'
-  task :restart do
-    admin.nginx_restart
-  end
-
-  after :publishing, 'deploy:restart'
-  after :finishing, 'deploy:cleanup'
+  after :publishing, "deploy:restart"
+  after :finishing, "deploy:cleanup"
 end
