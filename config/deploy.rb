2fcea984c07e1b73212388e0ae77945446c24f24
Florian Nitschmann <f.nitschmann@googlemail.com>
2014-12-09 11:34:13 +0100

    change for deployment settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,9 +4,9 @@ set :rbenv_ruby, "2.1.3"
 set :rack_env, :production
 
 set :user, "deploy"
-set :domain, "customers.projects.nitschmann.io"
-set :application, "customers_api"
-set :deploy_to, "/home/deploy/customers_api"
+set :domain, "shopdb_api.projects.nitschmann.io"
+set :application, "shopdb_api"
+set :deploy_to, "/home/deploy/shopdb_api"
 set :scm, "git"
 set :repo_url, "git@github.com:fnitschmann/json_api_sample.git"
 set :branch, "master"
@@ -14,7 +14,7 @@ set :branch, "master"
 set :keep_releases, 3
 
 set :linked_files, %w{config.yml}
-set :linked_dirs, %w{bin db tmp}
+set :linked_dirs, %w{bin db tmp log}
 
 namespace :deploy do
   desc "restart the application"
